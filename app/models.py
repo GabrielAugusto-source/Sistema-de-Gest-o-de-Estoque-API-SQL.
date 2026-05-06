f25ec71f2b02541a47ca4dc8693e5c681388c3e2
Gabriel Augusto <gabriel.augusto.diegues@gmail.com>
2026-05-06 15:55:37 -0300

    Continuação do esqueleto

---
+++ app/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Category(Base):
+    
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, unique=True)
+
+    products = relationship("Product", back_populates="category")
+
+    class Supplier(Base):
+        __tablename__ = "suppliers"
+
+        id = Column(Integer, primary_key=True, index=True)
+        name = Column(String, nullable=False)
+        contact = Column(String)
+
+        products = relationship("Product", back_populates="supplier")
+
+
+        class Product(Base):
+            __tablename__ = "products"
+
+            id = Column(Integer, primary_key=True, index=True)
+            name = Column(String, nullable=False)
+            quantity = Column(Integer, default=0)
+            price = Column(Float, nullable=False)
+            category_id = Column(Integer, ForeignKey("categories.id"))
+            supplier_id = Column(Integer, ForeignKey("suppliers.id"))
+
+            category = relationship("Category", back_populates="products")
+            supplier = relationship("Supplier", back_populates="products")
